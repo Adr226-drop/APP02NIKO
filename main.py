8f71ba5a4ad233752c1873511b3e14a6b21e98db
Adr226-drop <pedrozaromeroadriannicolas@cetis50cdmx.com>
2024-09-11 16:26:47 -0600

    Código de operaciones

---
+++ main.py
@@ -0,0 +1,22 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Suma y Resta UwU"
+    page.bgcolor="white"
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    
+    lbl1=ft.Text("Primer Número",
+                style=ft.TextStyle(size=40,weight="bold"))
+    
+    img1=ft.Image(scr="diavlo.png",width=200,height=200)
+    
+    btnSumar=ft.
+    
+
+
+
+
+
+ft.app(main)
